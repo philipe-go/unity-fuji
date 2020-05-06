ddcf35155089c39c481636fb651cbd54dd004df2
Phil[go] <philipe.ng@protonmail.com>
2020-05-06 10:46:21 -0400

    Initial Commit
    Pushing the game project to the github

---
+++ Assets/Scripts/loading.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+using System.Collections;
+
+public class loading : MonoBehaviour
+{
+    private AsyncOperation async;
+    private float waitTime = 10.0f;
+    [SerializeField] private Image progressBar;
+    [SerializeField] private Text progressText;
+
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        Input.ResetInputAxes();
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        async = SceneManager.LoadSceneAsync(currentScene.buildIndex + 1);
+        async.allowSceneActivation = false;
+        System.GC.Collect();
+    }
+
+    private void Update()
+    {
+        if (progressBar)
+        {
+            progressBar.fillAmount = async.progress + 0.1f;
+        }
+
+        if (progressText)
+        {
+            progressText.text = ((async.progress + 0.1f) * 100).ToString("f1") + " %";
+        }
+
+        if ((async.progress > 0.89f))
+        {
+            if (SplashScreen.isFinished) { async.allowSceneActivation = true; }
+        }
+    }
+
+    private YieldInstruction SplashScreenPause()
+    {
+        return new WaitForSeconds(waitTime);
+    }
+}
---
+++ README.md
@@ -0,0 +1,59 @@
+![LOGO](./Manual/phil(go)GitHubLogo.png?Style=centerme)
+
+# MY BLOCK BREAKER - UNITY ENGINE PROJECT #
+
+#######################################################################
+
+- 420-JV8-AS - GAME ENGINE I
+
+### Professor: ###
+
+- Marc-Andre Larouche
+
+### Development Team: ###
+
+- Philipe Gouveia
+
+#######################################################################
+
+
+**Section 1: About the Game**
+
+      This game is a type of blockbreaker with a tranquil atmosphere designed as first assignment for the course Game Engine I. This project was done during one week and is not totally complete. 
+
+
+**Section 2: About the Manual**
+
+
+      2.1 - Important Visuals:
+      
+![Iconography](./Manual/Iconography.png)
+
+
+**Section 3: Development Process**
+
+https://trello.com/b/18JjtMkI/7378-teami-2020
+
+
+**Section 4: References**
+
+* 4.0 - Game Design *
+
+ 
+bricks by CreativeGameAssets.com 
+ 
+background and characters by Philipe Gouveia 
+ 
+ 
+* 4.1 - PROGRAMMING *
+ 
+by Philipe Gouveia 
+ 
+* 4.2 - SOUND * 
+ 
+piano music by ShadyDave - freesound.com 
+ 
+
+* 4.3 - FONT *
+
+ Heroes Legend by imagex Wanadoo at dafont.com
